876c6b796e057017e124bc67e9de8904868372f6
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-08 17:59:36 +0530

    task 7 complete

---
+++ javaScript/Task-7.js
@@ -0,0 +1,22 @@
+// Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+
+let numbers = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+]
+
+numbers[1][0] = 99;
+
+console.log(numbers);
+
+
+
+/*
+Expected Array:
+[
+  [1, 2],
+  [99, 4],
+  [5, 6]
+] */
